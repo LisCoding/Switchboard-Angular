d6f2834ff0539b5c09a3d6aa1b0af9e193885582
Liseth Cardozo <cardozoliseth@gmail.com>
2017-10-11 14:47:43 -0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  switch_on :boolean = false;
+  switches: string[] = ["OFF", "OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF"];
+  color = 'red'
+  switchOn(idx) {
+    console.log("hey I was clicked", idx)
+    var value = this.switches[idx];
+    if(value == "OFF"){
+      this.switches[idx] = 'ON'
+      this.color = 'green'
+    } else {
+      this.switches[idx] = 'OFF'
+      this.color = 'red'
+    }
+  }
 }
